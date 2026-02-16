dccf2783cc11d6e67608fdbbeea0612a6073e155
Tyron Merrell <tyron.merrell@gmail.com>
2026-02-16 10:11:56 -0600

    Refactor complete.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,33 +4,24 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception, unless: -> { request.format.json? }
 
+  include AuthenticationConcerns
+
   # Backend user authentication helper methods
   def current_user
-    token = cookies.signed[:jwt]
-    return unless token
-
-      begin
-        decoded_token = generate_jwt_token(token)
-        User.find_by(id: decoded_token[0]["user_id"])
-      rescue JWT::ExpiredSignature
-        nil
-      end
-  end
-
-  def authenticate_user
-    return if current_user
-
-    rendor json: {}, status: :unauthorized
+    @current_user
   end
 
   private
 
-  def generate_jwt_token(token)
-    JWT.decode(
-          token,
-          Rails.application.credentials.fetch(:secret_key_base),
-          true,
-          { algorithm: "HS256" }
-        )
+  def set_current_user
+    @current_user = current_user
+  end
+
+  def require_login
+    redirect_to login_url, alert: "Please log in first" unless current_user
+  end
+
+  def require_admin
+    redirect_to root_url, alert: "You must be an admin to access this page." unless current_user&.admin?
   end
 end
