e9049537b318dea9931dfb4c7d7263dfb36d9852
Tyron Merrell <tyron.merrell@gmail.com>
2026-02-16 10:21:09 -0600

    Refactor complete.

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module AirbnbCloneBackend
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.autoload_paths << "#{config.root}/lib #{config.root}/app/controllers/concerns"
   end
 end
