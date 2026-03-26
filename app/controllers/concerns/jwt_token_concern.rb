a0a5096b739d5478c47f9adcdf35d5ad9db3e929
Tyron Merrell <tyron.merrell@gmail.com>
2026-03-25 21:49:29 -0500

    JWT logic moved to concerns.

--- app/controllers/concerns/jwt_token_concern.rb
+++ app/controllers/concerns/jwt_token_concern.rb
@@ -3,6 +3,17 @@ module JwtTokenConcern
 
   private
 
+  def generate_jwt_token(user)
+    JWT.encode(
+        {
+          user_id: user.id, # the data to encode
+          exp: 24.hours.from_now.to_i # the expiration time
+        },
+        jwt_secret_key, # the secret key
+        "HS256" # the encryption algorithm
+      )
+  end
+
   def jwt_secret_key
     Rails.application.credentials[:secret_key_base] || Rails.application.secret_key_base
   end
