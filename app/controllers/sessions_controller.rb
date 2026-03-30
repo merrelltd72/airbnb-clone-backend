c89d303268b2112a99c83bcee9696611d1f65bfd
Tyron Merrell <tyron.merrell@gmail.com>
2026-03-29 19:59:38 -0500

    moved and updated jwt logic.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,11 @@ class SessionsController < ApplicationController
 
   # Create user login session
   def create
-    user = User.find_by(email: params[:email])
-    if user && user.authenticate(params[:password])
+    user = User.find_by(email: params[:email].to_s.strip.downcase)
+    if user&.authenticate(params[:password])
       jwt = generate_jwt_token(user)
-      cookies.signed[:jwt] = { value: jwt, httponly: true }
-      render json: { email: user.email, user_id: user.id }, status: :created
+      cookies.signed[:jwt] = jwt_cookie_options.merge(value: jwt)
+      render json: { email: user.email, user_id: user.id }, status: :ok
     else
       render json: {}, status: :unauthorized
     end
@@ -17,7 +17,22 @@ class SessionsController < ApplicationController
 
   # Delete user login session
   def destroy
-    cookies.delete(:jwt)
+    cookies.delete(:jwt, jwt_cookie_delete_options)
     render json: { message: "Logged out successfully" }
+  end
+
+  private
+
+  def jwt_cookie_options
+    {
+      httponly: true,
+      expires: 24.hours.from_now,
+      secure: Rails.env.production?,
+      same_site: :lax
+    }
+  end
+
+  def jwt_cookie_delete_options
+    jwt_cookie_options.except(:expires)
   end
 end
