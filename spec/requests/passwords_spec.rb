8d85c18be60e6f92e7cc2e318d005a9aae986243
Tyron Merrell <tyron.merrell@gmail.com>
2026-04-28 22:34:11 -0500

    WIP

--- spec/requests/passwords_spec.rb
+++ spec/requests/passwords_spec.rb
@@ -1,17 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe "Passwords", type: :request do
-  describe "GET /forgot" do
-    it "returns http success" do
-      get "/passwords/forgot"
-      expect(response).to have_http_status(:success)
-    end
+  let(:json_headers) { { "CONTENT_TYPE" => "application/json", "ACCEPT" => "application/json" } }
+
+  let(:user) do
+    User.create!(
+      name: "Reset User",
+      email: "reset@example.com",
+      password: "password",
+      password_confirmation: "password"
+    )
   end
 
-  describe "GET /reset" do
-    it "returns http success" do
-      get "/passwords/reset"
-      expect(response).to have_http_status(:success)
-    end
+  it "forgot returns generic success for existing email" do
+    post "/password/forgot.json",
+    params: { email: user.email }.to_json,
+    headers: json_headers
+
+    expect(response).to have_http_status(:ok)
+    expect(JSON.parse(response.body)["message"]).to be_present
+  end
+
+  it "forgot returns generic success for unknown email" do
+    post "/password/forgot.json",
+      params: { email: "unknown@example.com" }.to_json,
+      headers: json_headers
+
+    expect(response).to have_http_status(:ok)
+    expect(JSON.parse(response.body)["message"]).to be_present
+  end
+
+  it "reset returns success for valid token" do
+    token = user.generate_reset_password_token!
+    post "/password/reset.json",
+    params: {
+      token: token,
+      new_password: "NewPassword123!",
+      new_password_confirmation: "NewPassword123!"
+    }.to_json,
+    headers: json_headers
+
+    expect(response).to have_http_status(:ok)
+    expect(JSON.parse(response.body)["message"]).to be_present
+  end
+
+  it "reset rejects invalid token" do
+    post "/password/reset.json",
+      params: {
+        token: "bad-token",
+        new_password: "NewPassword123!",
+        new_password_confirmation: "NewPassword123!"
+      }.to_json,
+      headers: json_headers
+
+    expect(response).to have_http_status(:bad_request)
   end
 end
